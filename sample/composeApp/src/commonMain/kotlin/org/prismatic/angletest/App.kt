66f30ee527a901a4ce25832a2ea33737f09a17cb
Reid <reid@prismatic.org>
2024-08-16 19:43:12 +0900

    chore: add `Opus` View

--- sample/composeApp/src/commonMain/kotlin/org/prismatic/angletest/App.kt
+++ sample/composeApp/src/commonMain/kotlin/org/prismatic/angletest/App.kt
@@ -1,39 +1,35 @@
 package org.prismatic.angletest
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import kotlinangle.sample.composeapp.generated.resources.Res
-import kotlinangle.sample.composeapp.generated.resources.compose_multiplatform
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.prismatic.kotlinangle.Greeting
-import org.prismatic.kotlinangle.getAngleWrapper
+import org.prismatic.opus.Opus
+import org.prismatic.opus.OpusView
+import org.prismatic.opus.visualrender.VisualRender
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        val angle = remember { getAngleWrapper() }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me! (${angle.callAngleApiTest()})")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
-    }
+	MaterialTheme {
+		Box(Modifier.padding(50.dp)) {
+			val color = Color.Red
+			val emptyOpus = remember {
+				Opus {
+					VisualRender {
+						onRender {
+							fillScreenRGB(color.red, color.green, color.blue)
+						}
+					}
+				}
+			}
+			OpusView(emptyOpus, Modifier.size(300.dp))
+		}
+	}
 }
